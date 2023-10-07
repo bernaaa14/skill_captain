9230c8cb9136001875f33ad8a8f575361d3acc09
bernaaa14 <131166848+bernaaa14@users.noreply.github.com>
2023-10-08 00:35:35 +0800

    Python 3 Day 2

---
+++ skill_captain_python_advanced/day_2/day_2.py
@@ -0,0 +1,23 @@
+import math_operations
+
+# Test math_operations module
+
+max_result = math_operations.maximum(4, 4)
+min_result = math_operations.minimum(10, -10)
+addition_result = math_operations.add(5, 10)
+subtraction_result = math_operations.subtract(5, 10)
+multiplication_result = math_operations.multiply(5, 10)
+division_result = math_operations.divide(12, 5)
+circumference_result = math_operations.circumference(3)
+power_result = math_operations.power(2, 5)
+deg_to_rad_result = math_operations.degrees_to_radians(70)
+
+print(max_result)
+print(min_result)
+print(addition_result)
+print(subtraction_result)
+print(multiplication_result)
+print(division_result)
+print(circumference_result)
+print(power_result)
+print(deg_to_rad_result)
