6538fbec124cc873fb4b9c03cfb09d7283cd5be8
Berna Alhambra <berna.alhambra@lpunetwork.edu.ph>
2023-10-28 00:23:14 +0800

    Added_django_directory

--- skill_captain_python_beginner/day_1.py
+++ skill_captain_python_beginner/day_1.py
@@ -1,8 +1,19 @@
-print("Hello world")
-print(Hello world)
-print("")
-print(Hello world)
+from typing import List
 
 
+def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    anagrams = {}  # dictionary to store the anagrams
+    for string in strs:  # Iterate over the list of strs
+        sorted_str = "".join(sorted(string))  # sort the string,concatenate into single string
 
+    if sorted_str not in anagrams:  # check if the sorted_Str is not yet in anagram
+        anagrams[sorted_str] = [string]  # if not, add the inputted string in anagram
+    else:
+        anagrams[sorted_str].append(string)  # group strings
+    return list(anagrams.values())  # teturns the values of the dictionary
+
+
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+result = groupAnagrams(strs)
+print(result)
 
